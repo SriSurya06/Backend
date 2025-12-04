0abc44da9d7d946c636440847d48b01b5575daa1
SriSurya06 <23MH1A05F2@acoe.edu.in>
2025-12-04 11:13:19 +0530

    result updated

--- 3.Async_Await.js
+++ 3.Async_Await.js
@@ -1,15 +1,14 @@
 function fun1(id){
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
-            resolve("Wingardium Levoisa")
+            resolve("Wingardium Levoisa") // Spell
         },2000);
     })
 };
 
 const fun2=async()=>{
-    console.log("Swish and Flick");
+    console.log("Swish and Flick"); //Wand Movement
     const res=await fun1("123");
     console.log(res);
-    console.log("Spell Successful");
 };
 fun2();
